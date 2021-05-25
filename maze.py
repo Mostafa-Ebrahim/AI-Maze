015e52d35f79520404ad6363e00d184fd58cc787
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2021-05-25 18:15:54 +0200

    add heuristic function

--- maze.py
+++ maze.py
@@ -43,17 +43,17 @@ class Maze():
         for i, row in enumerate(self.walls):
             for j, col in enumerate(row):
                 if col:
-                    # print("░", end="")
-                    print(" ▣ ", end="")
+                    print("░", end="")
+                    # print(" ▣ ", end="")
                 elif (i, j) == self.start:
-                    print(" S ", end="")
+                    print("S", end="")
                 elif (i, j) == self.goal:
-                    print(" G ", end="")
+                    print("G", end="")
                 elif solution is not None and (i, j) in solution:
                     print("•", end="")
                 else:
-                    # print(" ", end="")
-                    print(" □ ", end="")
+                    print(" ", end="")
+                    # print(" □ ", end="")
             print()
         print()
 
