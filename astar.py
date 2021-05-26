a56a1979937ccce528abc2fe5fbef72ddb0d9a40
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2021-05-26 21:05:08 +0200

    init A*

--- astar.py
+++ astar.py
@@ -1,4 +1,41 @@
 from data_structure import *
+from publicFunctions import *
 
 def astar(self):
-    return
+    h=heu(self, self.start)
+    c=cost(self, self.start)
+    startNode = Node(state=self.start, parent=None, action=None, heuristic=h, cost=c)
+
+    availableNodes = []
+    self.visitedNodes = set()
+
+    availableNodes.append(startNode)
+
+    while True:
+        if len(availableNodes) == 0:
+            print("No solution")
+            break
+
+        currentNode = availableNodes.pop(getMinF(availableNodes))
+        # reached the goal ?
+        if currentNode.state == self.goal:
+                actions = []
+                cells = []
+                while currentNode.parent is not None:
+                    actions.append(currentNode.action)
+                    cells.append(currentNode.state)
+                    currentNode = currentNode.parent
+                actions.reverse()
+                cells.reverse()
+                self.solution = (actions, cells)
+                print ("The Final Cost: ", FinalCost(actions))
+                print("Actions: "+', '.join(actions))
+                return
+
+        self.visitedNodes.add(currentNode.state)
+
+        # Add available nodes
+        for action, state in self.availableActions(currentNode.state):
+                if state not in self.visitedNodes:
+                    child = Node(state=state, parent=currentNode, action=action, heuristic=heu(self, currentNode.state), cost=cost(self, currentNode.state))
+                    availableNodes.append(child)
