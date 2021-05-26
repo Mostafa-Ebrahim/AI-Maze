585001dbe9f9fd27d09710fe87c0230968310343
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2021-05-26 17:20:14 +0200

    implement final cost function

--- publicFunctions.py
+++ publicFunctions.py
@@ -1,18 +1,9 @@
 from data_structure import *
 
-OverAllCost = 0
-
 def heu(self, node):
     (r1, c1) = node
     (r2, c2) = self.goal
     return abs(r2 - r1) + abs (c2 - c1)
-
-def getcost(self):
-    global OverAllCost
-    if self.direction == self.action:
-        OverAllCost += 1
-    else:
-        OverAllCost += 2
 
 def getMinH(fringe):
     Min = 0
@@ -21,5 +12,11 @@ def getMinH(fringe):
             Min = i
     return Min
 
-def FinalCost():
-    return(OverAllCost//2 + 2)
+def FinalCost(actions):
+    cost = 0
+    for i in range(1, len(actions)):
+        if actions[i] == actions[i-1]:
+            cost+=1
+        else:
+            cost+=2
+    return cost+1
