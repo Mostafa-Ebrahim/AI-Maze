8634abb39f0187ce14f5fabedf4a4ca6bd62d0f6
Mohamed Moussa <mohamedmoussa9928@gmail.com>
2021-05-26 18:56:26 +0200

    using maze1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from publicFunctions import *
 def run():
     # if len(sys.argv) != 2:
     #     sys.exit("Usage: python main.py maze1.txt")
-    maze = 'maze2.txt'
+    maze = 'maze1.txt'
     m = Maze(maze)
     print("Maze: ")
     m.print()
